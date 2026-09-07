de6585c79af75caacb60e02a9569833c17a721fd
PocketSpace <xbu080675@gmail.com>
2026-09-07 11:32:23 +0800

    stage 0.7.7 version

---
+++ app/build.gradle.next.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.luming.tray"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.luming.tray"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 13
+        versionName = "0.7.7"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+}
