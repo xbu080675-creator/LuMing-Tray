3ba3852746acdfd3e3e46a83bd5137bc30e71732
PocketSpace <xbu080675@gmail.com>
2026-09-07 17:31:41 +0800

    build: bump LuMing to 0.17.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.luming.tray"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "0.17.0"
+        versionCode = 25
+        versionName = "0.17.1"
     }
 
     compileOptions {
