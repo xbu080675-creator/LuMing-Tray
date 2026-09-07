ae1281d3c7f4b4b63c9db151b7cadc3211cd8d6a
PocketSpace <xbu080675@gmail.com>
2026-09-07 20:55:40 +0800

    chore: bump settings crash fix to 0.17.6.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.luming.tray"
         minSdk = 26
         targetSdk = 35
-        versionCode = 33
-        versionName = "0.17.6.3"
+        versionCode = 34
+        versionName = "0.17.6.4"
     }
 
     signingConfigs {
