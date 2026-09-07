6f1a2217c4a1ff30b307f88d4d65df64d9737b4f
PocketSpace <xbu080675@gmail.com>
2026-09-07 15:00:35 +0800

    chore: bump LuMing Tray to 0.13.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.luming.tray"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "0.12.0"
+        versionCode = 20
+        versionName = "0.13.0"
     }
 
     compileOptions {
