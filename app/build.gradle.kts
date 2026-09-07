8339c0c9ed1ad96b75f593d1e44ddb17151cd47f
PocketSpace <xbu080675@gmail.com>
2026-09-07 10:47:44 +0800

    chore: bump version to 0.7.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.luming.tray"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.7.3"
+        versionCode = 10
+        versionName = "0.7.4"
     }
 
     compileOptions {
