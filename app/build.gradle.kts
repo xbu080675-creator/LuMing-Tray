e12036d9105f738e2108ce6133340615d27ac9a6
PocketSpace <xbu080675@gmail.com>
2026-09-07 14:31:08 +0800

    chore: bump model availability release to 0.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.luming.tray"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "0.11.0"
+        versionCode = 19
+        versionName = "0.12.0"
     }
 
     compileOptions {
