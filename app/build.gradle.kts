ff348fad189ec199640638d42a42946e597cba88
PocketSpace <xbu080675@gmail.com>
2026-09-07 12:48:57 +0800

    chore: bump cost intelligence release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.luming.tray"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.9.0"
+        versionCode = 17
+        versionName = "0.10.0"
     }
 
     compileOptions {
