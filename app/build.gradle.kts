50790e6c8b580117282354e995702a33acd036e6
PocketSpace <xbu080675@gmail.com>
2026-09-07 17:20:40 +0800

    chore: bump stable shell to 0.17.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.luming.tray"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "0.16.0"
+        versionCode = 24
+        versionName = "0.17.0"
     }
 
     compileOptions {
